2b9fc054a50e9f418c1283a8b127b177d3113dbc
Jesse Manninen <jesse.manninen@gmail.com>
2023-04-01 20:29:54 +0300

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
-import { Component } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
